c0205dae4251e594dd4aa5304f6d7418492ed168
Boyan <boyanminchev@gmail.com>
2017-04-29 11:43:15 +0300

    added firebase-config files

---
+++ scripts/firebase-config.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyDN7MURwETAgvb6mXP30qoplJZr5255lCE",
+    authDomain: "biostore-c87aa.firebaseapp.com",
+    databaseURL: "https://biostore-c87aa.firebaseio.com",
+    projectId: "biostore-c87aa",
+    storageBucket: "biostore-c87aa.appspot.com",
+    messagingSenderId: "1087977620216"
+};
+firebase.initializeApp(config);
