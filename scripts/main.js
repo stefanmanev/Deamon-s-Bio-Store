37c5b1863d94e151d33b9fe02b1a469a6b3f7feb
bminchev <boyanminchev@gmail.com>
2017-04-30 14:21:25 +0300

    firebase

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@ import Bootstrap from 'bootstrap';
 import { getTemplate as getLogin } from 'loginController';
 import { getTemplate as getHome } from 'homeController';
 import { getTemplate as getRegister } from 'registerController';
-import { getTemplate as getAbout } from 'aboutController';
 
 var sammyApp = Sammy('#wrapper', function(){
     this.get('#/', function () {
@@ -22,10 +21,6 @@ var sammyApp = Sammy('#wrapper', function(){
 
     this.get('#/login', function() {
         return getLogin();
-    });
-
-    this.get('#/about', function() {
-        return getAbout();
     });
 
     this.get('#/user', function() {
